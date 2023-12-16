3d28d1fc1578b16f66db4fd719547d020bb78603
grayd173 <grayd@merrimack.edu>
2023-12-16 12:31:06 -0500

    fixed some controls issues

--- resources/Scripts/TestScript.cs
+++ resources/Scripts/TestScript.cs
@@ -6,11 +6,14 @@ public partial class TestScript : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Hello World");
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if(Input.IsActionPressed("mouse1")){
+			GD.Print("Mouse1 Triggered");
+		}
 	}
 }
